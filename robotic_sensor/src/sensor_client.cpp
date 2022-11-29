b106c29128778f4cbcae6ad7da49df522d846b78
jmasci2 <119371685+jmasci2@users.noreply.github.com>
2022-11-28 17:31:42 -0800

    Update sensor_client.cpp
    Remove test code

--- robotic_sensor/src/sensor_client.cpp
+++ robotic_sensor/src/sensor_client.cpp
@@ -68,10 +68,3 @@ int main(int argc, char **argv)
   rclcpp::shutdown();
   return 0;
 }
-
-
-std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("add_two_ints_client");
-  rclcpp::Client<example_interfaces::srv::AddTwoInts>::SharedPtr client =
-    node->create_client<example_interfaces::srv::AddTwoInts>("add_two_ints");
-
-  auto request = std::make_shared<example_interfaces::srv::AddTwoInts::Request>();
